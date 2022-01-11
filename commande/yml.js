06299e3db33d376a303e822d776d2f9a43cc2756
ToniPortal <38785425+Bouftout@users.noreply.github.com>
2022-01-11 09:57:26 +0100

    Update yml.js

--- commande/yml.js
+++ commande/yml.js
@@ -3,29 +3,30 @@ exports.run = (client, message, args, colors) => {
     const yaml = require('js-yaml');
     const fs = require('fs');
 
+var file = `./data/exp/${message.author.id}.yml`;
 
-    fs.exists(`./data/test.yml`, function(exists) {
+    fs.exists(file, function (exists) {
 
         if (exists) {
 
 
-            var test = yaml.load(fs.readFileSync(`./data/test.yml`, 'utf8'));
+            var test = yaml.load(fs.readFileSync(file, 'utf8'));
 
-
-
-
-            let data = { test: 1 };
+            let data = { exp: test.exp+1 };
 
             let yamlStr = yaml.dump(data);
-            fs.writeFileSync('./data/test.yml', yamlStr, 'utf8');
+            fs.writeFileSync(file, yamlStr, 'utf8');
 
         } else {
 
 
-            var stream = fs.createWriteStream(`./data/test.yml`);
-            stream.once('open', function(fd) {
+            var stream = fs.createWriteStream(file);
+            stream.once('open', function (fd) {
                 stream.write(`name: ${message.author.username}\n`);
-                stream.write(`test: 0\n`);
+                stream.write(`exp: 0,\n`);
+                stream.write(`lvl: 0,\n`);
+                stream.write(`lvlup: 0,\n`);
+                stream.write(`image: "./image/exp/wallpaper.png"\n`);
                 stream.end();
             });
 
