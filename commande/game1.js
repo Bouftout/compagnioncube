2d47b0216583a2dfa9cb95a37aa284b5e6b74bf1
ToniPortal <38785425+Bouftout@users.noreply.github.com>
2022-01-17 17:35:34 +0100

    Update game1.js

--- commande/game1.js
+++ commande/game1.js
@@ -1,5 +1,5 @@
-exports.run = (client, message, args) => {
-
+exports.run = (client, message, args, colors) => {
+  
   //Jeu de carte contre bot
 
 
@@ -33,13 +33,11 @@ exports.run = (client, message, args) => {
 
     if (exists) {
 
-      var fichier = yaml.safeLoad(fs.readFileSync(`./data/game/game1/${message.author.id}.yml`, 'utf8'));
+      var fichier = yaml.load(fs.readFileSync(`./data/game/game1/${message.author.id}.yml`, 'utf8'));
 
       if (!args[0] || args[0] == "help" || args[0] == " ") {
-
-        message.channel.send({
-          embed: {
-            color: 0x970cd2,
+              const embed =  {
+            color: colors.info,
             author: {
               name: client.user.username,
               icon_url: client.user.avatarURL
@@ -52,8 +50,9 @@ exports.run = (client, message, args) => {
               text: `©ToniPortal#8330`
             }
           }
-        })
-
+                }
+        
+message.channel.send({ embeds: [embed] })
       }
 
 
@@ -61,51 +60,53 @@ exports.run = (client, message, args) => {
 
         if (args[1] == "1") { // poser la carte 1
 
+          if (fichier.un.morte == "0" || fichier.un.poser == "0") {
+          
           stream.once('open', function (fd) {
             stream.write(`un: {\n`);
-            stream.write(`un: ${fichier.un.un}\n`);
+            stream.write(`vie: ${fichier.un.vie}\n`);
             stream.write(`stat: ${fichier.un.stat}\n`);
             stream.write(`morte: ${fichier.un.morte}\n`);
-            stream.write(`poser: oui\n`);
+            stream.write(`poser: 1\n`);
             stream.write(`}\n`);
 
             stream.write(`deux: {\n`);
-            stream.write(`deux: ${fichier.deux.deux}\n`);
+            stream.write(`vie: ${fichier.deux.vie}\n`);
             stream.write(`stat: ${fichier.deux.stat}\n`);
             stream.write(`morte: ${fichier.deux.morte}\n`);
             stream.write(`poser: ${fichier.deux.poser}\n`);
             stream.write(`}\n`);
 
             stream.write(`trois: {\n`);
-            stream.write(`trois: ${fichier.trois.trois}\n`);
+            stream.write(`vie: ${fichier.trois.vie}\n`);
             stream.write(`stat: ${fichier.trois.stat}\n`);
             stream.write(`morte: ${fichier.trois.morte}\n`);
             stream.write(`poser: ${fichier.trois.poser}\n`);
             stream.write(`}\n`);
 
             stream.write(`quatre: {\n`);
-            stream.write(`quatre: ${fichier.quatre.quatre}\n`);
+            stream.write(`vie: ${fichier.quatre.vie}\n`);
             stream.write(`stat: ${fichier.quatre.stat}\n`);
             stream.write(`morte: ${fichier.quatre.morte}\n`);
             stream.write(`poser: ${fichier.quatre.poser}\n`);
             stream.write(`}\n`);
 
             stream.write(`cinq: {\n`);
-            stream.write(`cinq: ${fichier.cinq.cinq}\n`);
+            stream.write(`vie: ${fichier.cinq.vie}\n`);
             stream.write(`stat: ${fichier.cinq.stat}\n`);
             stream.write(`morte: ${fichier.cinq.morte}\n`);
             stream.write(`poser: ${fichier.cinq.poser}\n`);
             stream.write(`}\n`);
 
             stream.write(`six: {\n`);
-            stream.write(`six: ${fichier.six.six}\n`);
+            stream.write(`vie: ${fichier.six.vie}\n`);
             stream.write(`stat: ${fichier.six.stat}\n`);
             stream.write(`morte: ${fichier.six.morte}\n`);
             stream.write(`poser: ${fichier.six.poser}\n`);
             stream.write(`}\n`);
 
             stream.write(`sept: {\n`);
-            stream.write(`sept: ${fichier.sept.sept}\n`);
+            stream.write(`vie: ${fichier.sept.vie}\n`);
             stream.write(`stat: ${fichier.sept.stat}\n`);
             stream.write(`morte: ${fichier.sept.morte}\n`);
             stream.write(`poser: ${fichier.sept.poser}\n`);
@@ -117,6 +118,38 @@ exports.run = (client, message, args) => {
             stream.end();
           });
 
+                          const possagecarte1 = {
+                  color: colors.info,
+                  author: {
+                    name: client.user.username,
+                    icon_url: client.user.avatarURL()
+                  },
+                  title: `Vous venez de posez la carte 1 !`,
+                  description: `Stats :\nVie: ${fichier.un.vie}\n`,
+                  timestamp: new Date(),
+                  footer: {
+                    icon_url: client.user.avatarURL(),
+                    text: `©ToniPortal`
+                  }
+                }
+                          message.channel.send({ embeds: [possagecarte1] })
+          } else {
+                                      const dejaposercarte1 = {
+                  color: colors.error,
+                  author: {
+                    name: client.user.username,
+                    icon_url: client.user.avatarURL()
+                  },
+                  title: `Cette carte n'est pas utilisable !`,
+                  description: `Testez avec une autre carte ?`,
+                  timestamp: new Date(),
+                  footer: {
+                    icon_url: client.user.avatarURL(),
+                    text: `©ToniPortal`
+                  }
+                }
+                          message.channel.send({ embeds: [dejaposercarte1] })
+          }
         }
 
         /* Il manque a faire:
@@ -135,9 +168,10 @@ exports.run = (client, message, args) => {
 
     } else {
 
-      message.channel.send({
-        embed: {
-          color: 0x970cd2,
+
+
+                      const creationcarte = {
+          color: colors.info,
           author: {
             name: client.user.username,
             icon_url: client.user.avatarURL
@@ -147,60 +181,62 @@ exports.run = (client, message, args) => {
           timestamp: new Date(),
           footer: {
             icon_url: client.user.avatarURL,
-            text: `©ToniPortal#8330`
+            text: `©ToniPortal`
           }
         }
-      })
-
+                }
+                      message.channel.send({ embeds: [creationcarte] })
+                      
+      
       var stream = fs.createWriteStream(`./data/game/game1/${message.author.id}.yml`);
       stream.once('open', function (fd) {
         stream.write(`un: {\n`);
-        stream.write(`un: ${random(100, 9999)}\n`);
+        stream.write(`vie: ${random(100, 9999)}\n`);
         stream.write(`stat: ${attackoudef()}\n`);
-        stream.write(`morte: non\n`);
-        stream.write(`poser: non\n`);
+        stream.write(`morte: 0\n`);
+        stream.write(`poser: 0\n`);
         stream.write(`}\n`);
 
         stream.write(`deux: {\n`);
-        stream.write(`deux: ${random(100, 9999)}\n`);
+        stream.write(`vie: ${random(100, 9999)}\n`);
         stream.write(`stat: ${attackoudef()}\n`);
-        stream.write(`morte: non\n`);
-        stream.write(`poser: non\n`);
+        stream.write(`morte: 0\n`);
+        stream.write(`poser: 0\n`);
         stream.write(`}\n`);
 
         stream.write(`trois: {\n`);
-        stream.write(`trois: ${random(100, 9999)}\n`);
+        stream.write(`vie: ${random(100, 9999)}\n`);
         stream.write(`stat: ${attackoudef()}\n`);
-        stream.write(`morte: non\n`);
-        stream.write(`poser: non\n`);
+        stream.write(`morte: 0\n`);
+        stream.write(`poser: 0\n`);
         stream.write(`}\n`);
 
         stream.write(`quatre: {\n`);
-        stream.write(`quatre: ${random(100, 9999)}\n`);
+        stream.write(`vie: ${random(100, 9999)}\n`);
         stream.write(`stat: ${attackoudef()}\n`);
-        stream.write(`morte: non\n`);
-        stream.write(`poser: non\n`);
+        stream.write(`morte: 0\n`);
+        stream.write(`poser: 0\n`);
         stream.write(`}\n`);
 
         stream.write(`cinq: {\n`);
-        stream.write(`cinq: ${random(100, 9999)}\n`);
+        stream.write(`vie: ${random(100, 9999)}\n`);
         stream.write(`stat: ${attackoudef()}\n`);
-        stream.write(`morte: non\n`);
-        stream.write(`poser: non\n`);
+        stream.write(`morte: 0\n`);
+        stream.write(`poser: 0\n`);
         stream.write(`}\n`);
 
         stream.write(`six: {\n`);
-        stream.write(`six: ${random(100, 9999)}\n`);
+        stream.write(`vie: ${random(100, 9999)}\n`);
         stream.write(`stat: ${attackoudef()}\n`);
-        stream.write(`poser: non\n`);
-        stream.write(`poser: non\n`);
+        stream.write(`poser: 0\n`);
+        stream.write(`poser: 0\n`);
         stream.write(`}\n`);
 
         stream.write(`sept: {\n`);
-        stream.write(`sept: ${random(100, 9999)}\n`);
+        stream.write(`vie: ${random(100, 9999)}\n`);
         stream.write(`stat: ${attackoudef()}\n`);
-        stream.write(`morte: non\n`);
-        stream.write(`poser: non\n`);
+        stream.write(`morte: 0\n`);
+        stream.write(`poser: 0\n`);
         stream.write(`}\n`);
 
 
