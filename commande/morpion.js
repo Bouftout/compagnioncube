77790cd09d35865f98a95de4584083d56913a12d
ToniPortal <pastre.toni@gmail.com>
2023-12-26 16:58:10 +0100

    morp

--- commande/morpion.js
+++ commande/morpion.js
@@ -1,11 +1,22 @@
+const fs = require("fs")
+
 exports.run = (client, message, args, colors) => {
 
-  const morpion = require('morpionzerwyx');
-  const zerwyx = new morpion({
-    language: 'fr'
-  })
-  zerwyx.handleMessage(message)
+  // const final = morp.morpl1.join("") + "\n-----\n" + morp.morp2.join("") + "\n-----\n" + morp.morp3.join("")
+
+  if (args[1] == "start") {
 
 
+    const morp = {
+      "morpl1": ["0", "\u0020|\u0020", "0", "\u0020|\u0020", "0"],
+      "morpl2": ["0", "\u0020|\u0020", "0", "\u0020|\u0020", "0"],
+      "morpl3": ["0", "\u0020|\u0020", "0", "\u0020|\u0020", "0"]
+    }
+
+    const final = morp.morpl1.join("") + "\n--------\n" + morp.morpl2.join("") + "\n--------\n" + morp.morpl3.join("")
+
+    message.channel.send("Vous démmarer une partie contre le bot !\n" + final)
+
+  }
 
 } // fin
